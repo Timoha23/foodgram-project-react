95b1e004394e888c92902c517b1f3d2691844e5e
Ershov Timofey <eqeq.tima@yandex.ru>
2022-12-09 20:24:54 +0300

    2.14

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -3,7 +3,8 @@ from rest_framework import permissions
 
 class IsAdminOrAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.method in permissions.SAFE_METHODS or (
+        return (
+            request.method in permissions.SAFE_METHODS or
             obj.author == request.user
             or request.user.is_superuser
         )
